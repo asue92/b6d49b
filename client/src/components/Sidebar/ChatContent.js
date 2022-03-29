77d26ddb8120e35162f47259c396d375248cf138
Adam <adam.p.sue@gmail.com>
2022-03-29 19:29:37 +0000

    Styled notification badge and conditional rendering

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -7,6 +7,7 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "space-between",
     marginLeft: 20,
+    marginRight: 20,
     flexGrow: 1,
   },
   username: {
@@ -18,6 +19,13 @@ const useStyles = makeStyles((theme) => ({
     color: "#9CADC8",
     letterSpacing: -0.17,
   },
+  notificationBadge: {
+    fontSize: 12,
+    backgroundColor: '#3A8DFF',
+    width: 24,
+    height: 24,
+    marginTop: 10
+  }
 }));
 
 const ChatContent = ({ conversation }) => {
@@ -34,8 +42,9 @@ const ChatContent = ({ conversation }) => {
         <Typography className={classes.previewText}>
           {latestMessageText}
         </Typography>
-        {notificationCount}gio
-      </Box>
+      </Box>   
+      {notificationCount > 0 && <Avatar className={classes.notificationBadge}>{notificationCount}
+</Avatar>}
     </Box>
   );
 };
